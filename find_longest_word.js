1b83c67c03e23b576250d1117966d8bac108c9b6
Ciaran Darcy <ciaran.darcy1@gmail.com>
2016-02-12 17:02:01 +0000

    created generic operarator app

--- find_longest_word.js
+++ find_longest_word.js
@@ -1,9 +1,15 @@
 function find_longest(array){
 	var longest="";
+	var words=[];
 	for (i=0; i<array.length;i++){
 		if (array[i].length>longest.length){
+			words.pop();
 			longest=array[i];
+			words.push(array[i]);
+		}
+		else if(array[i].length===longest.length){
+			words.push(array[i]);
 		}
 	}
-	return longest.length;
+	return longest.length+' ' + words;
 }
